a85b06924af9bafa1fc9ec7219eb50e05dc2846e
yynka <dev@internetmagik.com>
2017-08-26 12:00:00 -0700

    added port scanning functionality to windows script

--- netscan.ps1
+++ netscan.ps1
@@ -1,5 +1,5 @@
 # netscan.ps1
-# this script scans the local network for active devices and retrieves their MAC addresses.
+# this script scans the local network for active devices, retrieves MAC addresses, and checks for open ports.
 
 # get the local IP address and subnet mask to figure out the network range
 function Get-NetworkRange {
@@ -25,16 +25,16 @@ function Scan-Network {
     for ($i = 1; $i -le 254; $i++) {
         $ip = "$ipBase$i"
         if (Test-Connection -ComputerName $ip -Count 1 -Quiet) {
-            Write-Host "$ip is active"
             $activeDevices += [PSCustomObject]@{
                 IPAddress = $ip
             }
+            Write-Host "$ip is active"
         }
     }
     return $activeDevices
 }
 
-# retrieve the MAC address of each active device using arp
+# retrieve MAC addresses for each active device using arp
 function Get-MACAddress {
     param (
         [array]$activeDevices
@@ -55,7 +55,44 @@ function Get-MACAddress {
     }
 }
 
-# main script execution
+# check for open common ports on each active device
+function Check-OpenPorts {
+    param (
+        [array]$activeDevices
+    )
+
+    $commonPorts = @{
+        21  = "FTP"
+        22  = "SSH"
+        23  = "Telnet"
+        25  = "SMTP"
+        53  = "DNS"
+        80  = "HTTP"
+        110 = "POP3"
+        143 = "IMAP"
+        443 = "HTTPS"
+        3389 = "RDP"
+    }
+
+    Write-Host "`nchecking for open ports on active devices..."
+    foreach ($device in $activeDevices) {
+        Write-Host "`n$($device.IPAddress)"
+        foreach ($port in $commonPorts.Keys) {
+            try {
+                $tcpClient = New-Object System.Net.Sockets.TcpClient
+                $tcpClient.Connect($device.IPAddress, $port)
+                $tcpClient.Close()
+                Write-Host "Port $port is open - $($commonPorts[$port])"
+                $device | Add-Member -MemberType NoteProperty -Name OpenPorts -Value "$port: $($commonPorts[$port])"
+            } catch {
+                # port is closed, no action needed
+            }
+        }
+    }
+}
+
+# main script execution: get the network range, scan for devices, retrieve MAC addresses, and check open ports
 $networkRange = Get-NetworkRange
 $activeDevices = Scan-Network -networkRange $networkRange
 Get-MACAddress -activeDevices $activeDevices
+Check-OpenPorts -activeDevices $activeDevices
